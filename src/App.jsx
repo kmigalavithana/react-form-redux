0b379e2cbda2a06e13010418a456167c48aa79ec
Kavindu Methmal <136598207+kmigalavithana@users.noreply.github.com>
2025-04-05 20:47:34 +0530

    config react router and root routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Routers from "./routers/Routers.jsx";
 
 const App = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <Routers/>
         </div>
     );
 };
